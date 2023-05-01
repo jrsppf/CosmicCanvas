ad74e8b7f2d36ea0589265385c193d43b54b0f38
jrsppf <46720358+jrsppf@users.noreply.github.com>
2023-05-01 17:13:10 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+document.querySelector("button").addEventListener("click", getFetch);
+
+function getFetch() {
+  const API_KEY = "FzyCmLx9NoJm0vdcacfoSKNNyZvAbcV6oZbu4tW5";
+  const date = document.querySelector(".date").value;
+
+  const url = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${date}`;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => displayResults(data))
+    .catch((error) => console.log(`error is ${error}`));
+}
+
+function displayResults(data) {
+  if (data.media_type === "image") {
+    document.querySelector(".image").src = data.hdurl;
+    document.querySelector(".video").classList.add("hidden");
+    document.querySelector(".video-container").classList.add("hidden");
+    document.querySelector(".image").classList.remove("hidden");
+    document.querySelector(".image-container").classList.remove("hidden");
+  } else if (data.media_type === "video") {
+    document.querySelector(".video").src = data.url;
+    document.querySelector(".image").classList.add("hidden");
+    document.querySelector(".image-container").classList.add("hidden");
+    document.querySelector(".video").classList.remove("hidden");
+    document.querySelector(".video-container").classList.remove("hidden");
+  }
+  document.querySelector(".title").innerHTML = data.title;
+  document.querySelector(".explanation").innerHTML = data.explanation;
+}
